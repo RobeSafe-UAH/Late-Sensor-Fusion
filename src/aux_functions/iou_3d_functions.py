136593273a3172313b0672d708fb99677a23c794
Cram3r95 <cram3r95@gmail.com>
2021-06-24 18:31:48 +0200

    Working on 3D IoU (Centroid must be in camera frame)

--- src/aux_functions/iou_3d_functions.py
+++ src/aux_functions/iou_3d_functions.py
@@ -1,34 +1,36 @@
-import numpy as np
 from scipy.spatial import ConvexHull
-from numpy import *
+import numpy as np
+from t4ac_msgs.msg import Node
 
-def intersect_bbox_with_yaw(box_a, box_b):
+def boxoverlap(a,b,criterion="union"):
     """
-    A simplified calculation of 3d bounding box intersection.
-    It is assumed that the bounding box is only rotated
-    around Z axis (yaw) from an axis-aligned box.
-    :param box_a, box_b: obstacle bounding boxes for comparison
-    :return: intersection volume (float)
+        boxoverlap computes intersection over union for bbox a and b in KITTI format.
+        If the criterion is 'union', overlap = (a inter b) / a union b).
+        If the criterion is 'a', overlap = (a inter b) / a, where b should be a dontcare area.
     """
-    # height (Z) overlap
-    min_h_a = np.min(box_a[2])
-    max_h_a = np.max(box_a[2])
-    min_h_b = np.min(box_b[2])
-    max_h_b = np.max(box_b[2])
-    max_of_min = np.max([min_h_a, min_h_b])
-    min_of_max = np.min([max_h_a, max_h_b])
-    z_intersection = np.max([0, min_of_max - max_of_min])
-    if z_intersection == 0:
-        return 0.
+    
+    x1 = max(a.x1, b.x1)
+    y1 = max(a.y1, b.y1)
+    x2 = min(a.x2, b.x2)
+    y2 = min(a.y2, b.y2)
 
-    # oriented XY overlap
-    xy_poly_a = Polygon(zip(*box_a[0:2, 0:4]))
-    xy_poly_b = Polygon(zip(*box_b[0:2, 0:4]))
-    xy_intersection = xy_poly_a.intersection(xy_poly_b).area
-    if xy_intersection == 0:
-        return 0.
+    w = x2-x1
+    h = y2-y1
 
-    return z_intersection * xy_intersection
+    if w<=0. or h<=0.:
+        return 0.
+    inter = w*h
+    aarea = (a.x2-a.x1) * (a.y2-a.y1)
+    barea = (b.x2-b.x1) * (b.y2-b.y1)
+    # intersection over union overlap
+    if criterion.lower()=="union":
+        o = inter / float(aarea+barea-inter)
+    elif criterion.lower()=="a":
+        o = float(inter) / float(aarea)
+    else:
+        raise TypeError("Unkown type for criterion")
+    return o
+
 
 def polygon_clip(subjectPolygon, clipPolygon):
    """ Clip a polygon with another polygon.
@@ -75,6 +77,7 @@ def polygon_clip(subjectPolygon, clipPolygon):
           return None
    return(outputList)
 
+
 def poly_area(x,y):
     """ Ref: http://stackoverflow.com/questions/24467972/calculate-area-of-polygon-given-x-y-coordinates """
     return 0.5*np.abs(np.dot(x,np.roll(y,1))-np.dot(y,np.roll(x,1)))
@@ -98,12 +101,7 @@ def box3d_vol(corners):
     c = np.sqrt(np.sum((corners[0,:] - corners[4,:])**2))
     return a*b*c
 
-def is_clockwise(p):
-    x = p[:,0]
-    y = p[:,1]
-    return np.dot(x,np.roll(y,1))-np.dot(y,np.roll(x,1)) > 0
-
-def box3d_iou(corners1, corners2):
+def box3d_iou(corners1, corners2, criterion='union'):
     ''' Compute 3D bounding box IoU.
     Input:
         corners1: numpy array (8,3), assume up direction is negative Y
@@ -111,65 +109,81 @@ def box3d_iou(corners1, corners2):
     Output:
         iou: 3D bounding box IoU
         iou_2d: bird's eye view 2D bounding box IoU
-    todo (kent): add more description on corner points' orders.
+    todo (rqi): add more description on corner points' orders.
     '''
-    # corner points are in counter clockwise order
+    # corner points area in counter clockwise order
     rect1 = [(corners1[i,0], corners1[i,2]) for i in range(3,-1,-1)]
     rect2 = [(corners2[i,0], corners2[i,2]) for i in range(3,-1,-1)] 
-    
     area1 = poly_area(np.array(rect1)[:,0], np.array(rect1)[:,1])
     area2 = poly_area(np.array(rect2)[:,0], np.array(rect2)[:,1])
-   
     inter, inter_area = convex_hull_intersection(rect1, rect2)
-    iou_2d = inter_area/(area1+area2-inter_area)
+    
     ymax = min(corners1[0,1], corners2[0,1])
     ymin = max(corners1[4,1], corners2[4,1])
-
     inter_vol = inter_area * max(0.0, ymax-ymin)
-    
     vol1 = box3d_vol(corners1)
     vol2 = box3d_vol(corners2)
-    iou = inter_vol / (vol1 + vol2 - inter_vol)
+    if criterion.lower() == 'union':
+	    iou = inter_vol / (vol1 + vol2 - inter_vol)
+	    iou_2d = inter_area/(area1+area2-inter_area)
+    elif criterion.lower() == 'a':
+        iou = inter_vol / vol1
+        iou_2d = inter_area / area1
+    else:
+        raise TypeError("Unkown type for criterion")
     return iou, iou_2d
 
-# ----------------------------------
-# Helper functions for evaluation
-# ----------------------------------
+def roty(t):
+    ''' Rotation about the y-axis. '''
+    c = np.cos(t)
+    s = np.sin(t)
+    return np.array([[c,  0,  s],
+                     [0,  1,  0],
+                     [-s, 0,  c]])
 
-def get_3d_box(box_size, heading_angle, center):
-    ''' Calculate 3D bounding box corners from its parameterization.
-    Input:
-        box_size: tuple of (length,wide,height)
-        heading_angle: rad scalar, clockwise from pos x axis
-        center: tuple of (x,y,z)
-    Output:
-        corners_3d: numpy array of shape (8,3) for 3D box cornders
+def compute_box_3d(obj):
+    ''' Takes an object and a projection matrix (P) and projects the 3d
+        bounding box into the image plane.
+        Returns:
+            corners_2d: (8,2) array in left image coord.
+            corners_3d: (8,3) array in LiDAR coord.
     '''
-    def roty(t):
-        c = np.cos(t)
-        s = np.sin(t)
-        return np.array([[c,  0,  s],
-                         [0,  1,  0],
-                         [-s, 0,  c]])
+    # compute rotational matrix around yaw axis
+    yaw = obj[1]
+    R = roty(yaw)  
 
-    R = roty(heading_angle)
-    l,w,h = box_size
+    # 3d bounding box dimensions
+    l = obj[0][2]
+    w = obj[0][1]
+    h = obj[0][0]
+    
+    # 3d bounding box corners
     x_corners = [l/2,l/2,-l/2,-l/2,l/2,l/2,-l/2,-l/2];
-    y_corners = [h/2,h/2,h/2,h/2,-h/2,-h/2,-h/2,-h/2];
+    y_corners = [0,0,0,0,-h,-h,-h,-h];
     z_corners = [w/2,-w/2,-w/2,w/2,w/2,-w/2,-w/2,w/2];
+    
+    # rotate and translate 3d bounding box
     corners_3d = np.dot(R, np.vstack([x_corners,y_corners,z_corners]))
-    corners_3d[0,:] = corners_3d[0,:] + center[0];
-    corners_3d[1,:] = corners_3d[1,:] + center[1];
-    corners_3d[2,:] = corners_3d[2,:] + center[2];
-    corners_3d = np.transpose(corners_3d)
-    return corners_3d
+    #print corners_3d.shape
+    corners_3d[0,:] = corners_3d[0,:] + obj[2][0]
+    corners_3d[1,:] = corners_3d[1,:] + obj[2][1]
+    corners_3d[2,:] = corners_3d[2,:] + obj[2][2]
+    # print('cornsers_3d: ', corners_3d)
+
+    return np.transpose(corners_3d)
+
+def box3doverlap(aa, bb, criterion='union'):
+    
+    aa_3d = compute_box_3d(aa)
+    bb_3d = compute_box_3d(bb)
+
+    iou3d, iou2d = box3d_iou(aa_3d, bb_3d, criterion=criterion)
+
+    return iou3d
 
 if __name__=='__main__':
-    print('------------------')
-    # get_3d_box(box_size, heading_angle, center)
-    corners_3d_ground  = get_3d_box((1.497255,1.644981, 3.628938), -1.531692, (2.882992 ,1.698800 ,20.785644)) 
-    corners_3d_predict  = get_3d_box((1.497255,1.644981, 3.628938), -1.531692, (2.882992 ,1.698800 ,20.785644)) 
-    # corners_3d_predict  = get_3d_box((1.497255,1.644981, 3.628938), -1.531692, (2.882992 ,1.698800 ,20.785644)) 
-    # corners_3d_predict = get_3d_box((1.458242, 1.604773, 3.707947), -1.549553, (2.756923, 1.661275, 20.943280 ))
-    (IOU_3d,IOU_2d)=box3d_iou(corners_3d_predict,corners_3d_ground)
-    print (IOU_3d,IOU_2d) #3d IoU/ 2d IoU of BEV(bird eye's view)
+    aa  = ((1.497255,1.644981, 3.628938), -1.531692, (1.2, 1.56, 23.56)) 
+    bb  = ((1.458242, 1.604773, 3.707947), -1.549553, (1.37, 1.67, 29.57))
+
+    iou3d = box3doverlap(aa,bb)
+    print("IoU 3D: ", iou3d)
